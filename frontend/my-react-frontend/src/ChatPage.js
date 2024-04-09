40184c50a18439dd004f9631095145edee050e4e
sw1nd <ahmetkucukdag@Ahmets-MacBook-Air.local>
2024-04-09 17:51:04 +0300

    update chat

--- frontend/my-react-frontend/src/ChatPage.js
+++ frontend/my-react-frontend/src/ChatPage.js
@@ -265,7 +265,7 @@ function ChatPage() {
           {!currentTitle && (
             <div className="empty-chat-container">
               <img
-                src="images/chatgpt-logo.svg"
+                src="../images/logo.png"
                 width={45}
                 height={45}
                 alt="ChatGPT"
@@ -300,7 +300,7 @@ function ChatPage() {
                         <BiSolidUserCircle size={28.8} />
                       </div>
                     ) : (
-                      <img src="images/chatgpt-logo.svg" alt="ChatGPT" />
+                      <img src="../images/logo.png" alt="ChatGPT" />
                     )}
                     {isUser ? (
                       <div>
