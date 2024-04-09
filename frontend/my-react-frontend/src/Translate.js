4d31f1b3cc93bc2123463b1996e7a8ec8406c3eb
heyitsyigit <yigit.yelmenoglu@gmail.com>
2024-04-09 21:51:25 +0300

    Update css and translate

--- frontend/my-react-frontend/src/Translate.js
+++ frontend/my-react-frontend/src/Translate.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import './css/translate.css'; // Make sure to adapt your CSS for React
+import { useAuth } from './AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import config from './config';
+
 
 function Translate() {
   const [sourceText, setSourceText] = useState('');
   const [translatedText, setTranslatedText] = useState('');
   const [sourceLanguage, setSourceLanguage] = useState('en');
-  const [targetLanguage, setTargetLanguage] = useState('en');
+  const [targetLanguage, setTargetLanguage] = useState('tr');
 
   const languages = [
     { code: 'en', name: 'English' },
@@ -17,6 +22,8 @@ function Translate() {
     { code: 'tr', name: 'Turkish' }
     // Add other languages here
   ];
+  const navigate = useNavigate();
+  const { login } = useAuth();
 
   const handleTranslate = () => {
     fetch('http://localhost:5000/translate_text', {
@@ -39,40 +46,52 @@ function Translate() {
     });
   };
 
+  const swapLanguages = () => {
+
+  };
+  const handleBack = () => {
+    navigate('/'); // Navigates the user to the previous page
+  };
+
   return (
-    <div className="translate-container">
-      <textarea
-        id="sourceText"
-        placeholder="Enter text to translate..."
-        value={sourceText}
-        onChange={(e) => setSourceText(e.target.value)}
-      />
-      <select
-        id="sourceLanguage"
-        value={sourceLanguage}
-        onChange={(e) => setSourceLanguage(e.target.value)}
-      >
+    
+  <body className="translate-container">
+        <textarea className = "null" id="null" disabled/>
+    <header>
+                <nav>
+                    <span onClick={handleBack} className="nav-text">{config.projectName}</span>
+                    <Link to="/home" className="nav-link">Main Page</Link>
+                </nav>
+            </header>
+    <div className="translate-container1">
+     <div className ="select-container-top"> 
+      <div classname="select-container">
+        <select id="sourceLanguage" value={sourceLanguage} onChange={(e) => setSourceLanguage(e.target.value)}>
         {languages.map(lang => (
-          <option key={lang.code} value={lang.code}>{lang.name}</option>
-        ))}
-      </select>
-      <select
-        id="targetLanguage"
-        value={targetLanguage}
-        onChange={(e) => setTargetLanguage(e.target.value)}
-      >
-        {languages.map(lang => (
-          <option key={lang.code} value={lang.code}>{lang.name}</option>
-        ))}
-      </select>
-      <button id="translateButton" onClick={handleTranslate}>Translate</button>
-      <textarea
-        id="translatedText"
-        placeholder="Translation will appear here..."
-        value={translatedText}
-        readOnly
-      />
+            <option key={lang.code} value={lang.code}>{lang.name}</option>
+          ))}
+        </select>
+      </div>
+      <div classname="select-container">
+        <select id="targetLanguage" value={targetLanguage} onChange={(e) => setTargetLanguage(e.target.value)}>
+          {languages.map(lang => (
+            <option key={lang.code} value={lang.code}>{lang.name}</option>
+          ))}
+        </select>
+      </div>
+     </div>
+     <div className='text-container-top'>
+      <div className='text-container'>
+      <textarea id="sourceText" placeholder="Enter text to translate..." value={sourceText}  onChange={(e) => {
+                                                                                                                setSourceText(e.target.value) 
+                                                                                                                handleTranslate()}}/>
+      </div>
+      <div className='text-container'>
+      <textarea id="translatedText" placeholder="Translation" value={translatedText} disabled readOnly/>
+      </div>
+      </div>
     </div>
+</body >
   );
 }
 
